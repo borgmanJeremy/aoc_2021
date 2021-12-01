7611c4c7bb3e1cb91e83969f7fa38fe702a9fb3a
Jeremy Borgman <borgman.jeremy@pm.me>
2021-12-01 09:12:01 -0600

    Day 1 with iterators

---
+++ day_1/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn read_from_file(path: &str) -> Vec<i32> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut data = Vec::new();
+    for line in reader.lines() {
+        match line {
+            Ok(text) => {
+                data.push(text.parse::<i32>().unwrap());
+            }
+            Err(err) => {
+                println!("Error reading line:{} ", err);
+            }
+        }
+    }
+    data
+}
+
+fn main() {
+    let input_data = read_from_file("/Users/jeremy/github/aoc_2021/day_1/input/input_1.txt");
+
+    let part_1: i32 = input_data
+        .windows(2)
+        .map(|window| if window[0] < window[1] { 1 } else { 0 })
+        .sum();
+
+    println!("part 1: {}", part_1);
+
+    let part_2: i32 = input_data
+        .windows(3)
+        .map(|w| w[0] + w[1] + w[2])
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .map(|w| if w[0] < w[1] { 1 } else { 0 })
+        .sum();
+
+    println!("part 2: {:?}", part_2);
+}
