8525639bd08fa77399c7f0a42b41cb3c0252919b
Jeremy Borgman <borgman.jeremy@pm.me>
2021-12-06 09:17:19 -0600

    part 1

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,16 +1,50 @@
+use std::cmp;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct LineSegment {
     p1: Point,
     p2: Point,
+}
+
+impl LineSegment {
+    fn is_vertical(&self) -> bool {
+        self.p1.x == self.p2.x
+    }
+
+    fn is_horizontal(&self) -> bool {
+        self.p1.y == self.p2.y
+    }
+
+    fn is_straight(&self) -> bool {
+        self.is_vertical() || self.is_horizontal()
+    }
+
+    fn extend(&self) -> Option<Vec<Point>> {
+        let mut full_line = Vec::new();
+        if self.is_horizontal() {
+            for x in cmp::min(self.p1.x, self.p2.x)..=cmp::max(self.p1.x, self.p2.x) {
+                full_line.push(Point { x, y: self.p1.y });
+            }
+            return Some(full_line);
+        } else if self.is_vertical() {
+            println!("{} {} ", self.p1.y, self.p2.y);
+            for y in cmp::min(self.p1.y, self.p2.y)..=cmp::max(self.p1.y, self.p2.y) {
+                println!("{}", y);
+                full_line.push(Point { x: self.p1.x, y });
+            }
+            return Some(full_line);
+        }
+        None
+    }
 }
 
 fn read_from_file(path: &str) -> Vec<LineSegment> {
@@ -50,7 +84,43 @@ fn read_from_file(path: &str) -> Vec<LineSegment> {
 }
 
 fn main() {
-    let line_list = read_from_file("input/sample.txt");
+    let line_list = read_from_file("input/input.txt");
 
-    println!("{:?}", line_list);
+    let max_x = line_list
+        .iter()
+        .map(|p| cmp::max(p.p1.x, p.p2.x))
+        .max()
+        .unwrap();
+
+    let max_y = line_list
+        .iter()
+        .map(|p| cmp::max(p.p1.y, p.p2.y))
+        .max()
+        .unwrap();
+
+    let mut map = HashMap::new();
+    for line in &line_list {
+        if let Some(full_line) = line.extend() {
+            for point in full_line {
+                match map.get(&point) {
+                    Some(val) => {
+                        map.insert(point.clone(), val + 1);
+                    }
+                    None => {
+                        map.insert(point.clone(), 1);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut count = 0;
+    for (_, val) in &map {
+        if *val >= 2 {
+            count += 1;
+        }
+    }
+
+    println!("{:?}", map);
+    println!("count: {:?}", count);
 }
