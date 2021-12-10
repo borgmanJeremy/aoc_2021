ec8c66c419268f5f9b0e9cc2958dda8dc3fe09de
Jeremy Borgman <borgman.jeremy@pm.me>
2021-12-10 11:40:13 -0600

    part 2 working

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -45,11 +45,39 @@ fn find_improper_close(input: &str) -> Option<char> {
     None
 }
 
+// Asummes not run on improper closes
+fn find_proper_close(input: &str) -> Vec<char> {
+    let mut open_stack = Vec::new();
+    for c in input.chars() {
+        if is_opening(c) {
+            open_stack.push(c);
+        }
+        if is_closing(c) && is_proper_close(*open_stack.last().unwrap(), c) {
+            open_stack.pop();
+        }
+    }
+
+    let mut close_stack: Vec<char> = open_stack
+        .iter()
+        .map(|c| match c {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            '<' => '>',
+            _ => panic!("not a proper open"),
+        })
+        .collect();
+
+    close_stack.reverse();
+    println!("{:?}", close_stack);
+    close_stack
+}
+
 fn main() {
     let input = read_from_file("input/input.txt");
 
-    let mut score = 0;
-
+    // Part 1
+    let mut score: i64 = 0;
     for line in &input {
         if let Some(c) = find_improper_close(line) {
             match c {
@@ -62,4 +90,26 @@ fn main() {
         };
     }
     println!("score: {}", score);
+
+    let mut score_list = Vec::new();
+    // Part 2
+    for line in &input {
+        score = 0;
+        if find_improper_close(line).is_none() {
+            find_proper_close(line).iter().for_each(|c| match c {
+                ')' => score = score * 5 + 1,
+                ']' => score = score * 5 + 2,
+                '}' => score = score * 5 + 3,
+                '>' => score = score * 5 + 4,
+                _ => {}
+            });
+
+            score_list.push(score);
+        }
+    }
+    score_list.sort_unstable();
+    println!("score: {:?}", score_list);
+
+    let mid = score_list.len() / 2;
+    println!("score: {:?}", score_list[mid]);
 }
